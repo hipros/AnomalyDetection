0b780d5356b230f770d846cf27fa9a3e0cb66bd7
hipros <npclinic3@gmailc.om>
2020-12-11 01:29:12 +0900

    AutoEncoder model

--- model.py
+++ model.py
@@ -1,52 +1,97 @@
 from torch import nn
-import math
+from torch.nn import functional as F
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.conv_1 = nn.Conv2d(in_ch, in_ch, 3, 1, 1, bias=True)
+        self.conv_2 = nn.Conv2d(in_ch, out_ch, 4, 2, 1, bias=True)
+        self.norm_1 = nn.BatchNorm2d(num_features=in_ch)
+        self.norm_2 = nn.BatchNorm2d(num_features=out_ch)
+
+    def forward(self, x):
+        x = self.conv_1(x)
+        x = self.norm_1(x)
+        x = F.leaky_relu(x, 0.2)
+
+        x = self.conv_2(x)
+        x = self.norm_2(x)
+        x = F.leaky_relu(x, 0.2)
+
+        return x
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.deconv_1 = nn.ConvTranspose2d(in_ch, out_ch, 4, 2, 1, bias=True)
+        self.deconv_2 = nn.ConvTranspose2d(out_ch, out_ch, 3, 1, 1, bias=True)
+        self.norm_1 = nn.BatchNorm2d(out_ch)
+        self.norm_2 = nn.BatchNorm2d(out_ch)
+
+    def forward(self, x):
+        x = self.deconv_1(x)
+        x = self.norm_1(x)
+        x = F.leaky_relu(x, 0.2)
+
+        x = self.deconv_2(x)
+        x = self.norm_2(x)
+        x = F.leaky_relu(x, 0.2)
+
+        return x
+
+
+class MlpBlock(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.mlp = nn.Linear(in_ch, out_ch)
+        self.norm = nn.BatchNorm1d(out_ch)
+
+    def forward(self, x):
+        x = self.mlp(x)
+        x = self.norm(x)
+        x = F.leaky_relu(x, 0.2)
+
+        return x
 
 
 class CAE(nn.Module):
     def __init__(self):
         super(CAE, self).__init__()
-        chpivet = 2
-        mlp_depth = 1
-        self.hidden_dim = 512
+        self.input_img_size = 64
+        self.ae_layer_num = 3
+        self.mlp_layer_num = 1
+
+        self.input_img_element = self.input_img_size ** 2
+        self.hidden_dim = self.input_img_size // 2 ** self.ae_layer_num
 
         encoder_module = []
         decoder_module = []
         mlp_module = []
 
-        encoder_module.append(nn.Conv2d(1, chpivet, 4, 2, 1))
-        encoder_module.append(nn.ReLU(True))
-        encoder_module.append(nn.BatchNorm2d(chpivet))
+        for i in range(self.ae_layer_num):
+            inc = 2 ** i
+            outc = 2 * inc
 
-        for i in range(int(math.log2(self.hidden_dim) - math.log2(chpivet))):
-            inc = chpivet*(2**i); outc = chpivet*(2**(i+1))
-            encoder_module.append(nn.Conv2d(inc, outc, 4, 2, 1))
-            encoder_module.append(nn.ReLU(True))
-            encoder_module.append(nn.BatchNorm2d(outc))
+            encoder_module.append(EncoderBlock(inc, outc))
 
-        for i in range(int(math.log2(self.hidden_dim) - math.log2(chpivet))-1, -1, -1):
-            inc = chpivet*(2**(i+1)); outc = chpivet*(2**i)
-            decoder_module.append(nn.ConvTranspose2d(inc, outc, 4, 2, 1))
-            decoder_module.append(nn.ReLU(True))
-            decoder_module.append(nn.BatchNorm2d(outc))
+        for i in range(self.ae_layer_num-1, -1, -1):
+            inc = 2 ** (i+1)
+            outc = inc // 2
 
-        decoder_module.append(nn.ConvTranspose2d(chpivet, 1, 4, 2, 1))
+            decoder_module.append(DecoderBlock(inc, outc))
 
-        for i in range(mlp_depth):
-            mlp_module.append(nn.Linear(self.hidden_dim, self.hidden_dim))
-            mlp_module.append(nn.ReLU(True))
-            mlp_module.append(nn.BatchNorm1d(self.hidden_dim))
+        for i in range(self.mlp_layer_num):
+            mlp_module.append(MlpBlock(self.hidden_dim, self.hidden_dim))
 
         self.encoder = nn.Sequential(*encoder_module)
         self.mlp = nn.Sequential(*mlp_module)
         self.decoder = nn.Sequential(*decoder_module)
+        self.flatten = nn.Flatten()
 
     def forward(self, x):
         x = self.encoder(x)
-
-        x = x.view(-1, self.hidden_dim)
-        x = self.mlp(x)
-        x = x.view(-1, self.hidden_dim, 1, 1)
-
         x = self.decoder(x)
 
         return x
