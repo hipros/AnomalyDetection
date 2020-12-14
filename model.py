327b601de42c24f5e23bb4babbc5fdafdec2d6c4
hipros <npclinic3@gmailc.om>
2020-12-15 04:55:41 +0900

    update Autoencoder model

--- model.py
+++ model.py
@@ -31,11 +31,11 @@ class EncoderBlock(nn.Module):
 class DecoderBlock(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(DecoderBlock, self).__init__()
-        self.deconv_1 = nn.ConvTranspose2d(in_ch, out_ch, 4, 2, 1, bias=True)
-        self.deconv_2 = nn.ConvTranspose2d(out_ch, out_ch, 3, 1, 1, bias=True)
-        self.deconv_3 = nn.ConvTranspose2d(out_ch, out_ch, 3, 1, 1, bias=True)
-        self.norm_1 = nn.BatchNorm2d(num_features=out_ch)
-        self.norm_2 = nn.BatchNorm2d(num_features=out_ch)
+        self.deconv_1 = nn.ConvTranspose2d(in_ch, in_ch, 4, 2, 1, bias=True)
+        self.deconv_2 = nn.ConvTranspose2d(in_ch, in_ch, 3, 1, 1, bias=True)
+        self.deconv_3 = nn.ConvTranspose2d(in_ch, out_ch, 3, 1, 1, bias=True)
+        self.norm_1 = nn.BatchNorm2d(num_features=in_ch)
+        self.norm_2 = nn.BatchNorm2d(num_features=in_ch)
         self.norm_3 = nn.BatchNorm2d(num_features=out_ch)
         self.lrelu = nn.LeakyReLU(0.2)
 
@@ -74,8 +74,10 @@ class CAE(nn.Module):
     def __init__(self):
         super(CAE, self).__init__()
         self.input_img_size = 64
-        self.ae_layer_num = 3
+        self.ae_layer_num = 4
         self.mlp_layer_num = 1
+        self.inc = 1
+        self.first_outc = 64
 
         self.input_img_element = self.input_img_size ** 2
         self.hidden_dim = self.input_img_size // 2 ** self.ae_layer_num
@@ -93,30 +95,43 @@ class CAE(nn.Module):
 
     def make_encoder_layer(self):
         layers = []
+        inc = self.inc
+        outc = self.first_outc
+
         for i in range(self.ae_layer_num):
-            inc = 2 ** i
-            outc = 2 * inc
+            if i == 0:
+                layers.append(EncoderBlock(inc, outc))
+            else:
+                layers.append(EncoderBlock(inc, outc))
 
-            layers.append(EncoderBlock(inc, outc))
+            inc = outc
+            outc = outc * 2
 
+        self.inc = inc
         return nn.Sequential(*layers)
 
     def make_decoder_layer(self):
         layers = []
+        inc = self.inc
+        outc = self.inc // 2
 
         for i in range(self.ae_layer_num-1, -1, -1):
-            inc = 2 ** (i+1)
-            outc = inc // 2
+            if i == 0:
+                layers.append(DecoderBlock(inc, 1))
+            else:
+                layers.append(DecoderBlock(inc, outc))
 
-            layers.append(DecoderBlock(inc, outc))
+            inc = outc
+            outc = inc // 2
 
         return nn.Sequential(*layers)
 
     def make_mlp_layer(self):
         layers = []
+        dim = self.hidden_dim * self.inc
 
         for i in range(self.mlp_layer_num):
-            layers.append(MlpBlock(self.hidden_dim, self.hidden_dim))
+            layers.append(MlpBlock(dim, dim))
 
         return nn.Sequential(*layers)
 
@@ -125,13 +140,12 @@ class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
         self.img_ch = 1
-        self.fc_ch = 1024
+        self.fc_ch = 512
 
-        self.represent_cfg = [64, 64, 'P', 128, 128, 'P',
-                              256, 256, 256, 256, 'P',
-                              512, 512, 512, 512, 'P',
-                              1024, 1024, 'P', 1024, 1024, 'P']
-        self.fc_cfg = [1024, 512, 256, 1]
+        self.represent_cfg = [32, 32, 'P', 64, 64, 'P',
+                              128, 128, 'P', 128, 128, 'P',
+                              256, 256, 'P', 512, self.fc_ch, 'P']
+        self.fc_cfg = [self.fc_ch, 256, 1]
         self.represent = self.make_represent_layers()
         self.fc = self.make_fc_layer()
 
